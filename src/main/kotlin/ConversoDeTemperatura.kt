58f5f195a3d6490a8b3c27740881f863c200481f
Ramon Lummertz <ramonsl@gmail.com>
2022-08-18 16:56:56 -0300

    Exercicios

---
+++ src/main/kotlin/ConversoDeTemperatura.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Fill in the code.
+}
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
---
+++ src/main/kotlin/PerfilDaInternet.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        // Fill in code
+    }
+}
